f9c5eded2bd84ad67d425cd5bf302f516608fb0c
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-10 20:05:31 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
-#include <stdio.h>  
-
-/**                                              * main - Entry point as the main function       *                                               * Return: Always 0                              */
+#include <stdio.h>
+/**
+ * main - Entry point as the main function
+ * Return: Always 0
+ */
 int main(void)
 {
-	char text[] = "\"with proper grammar, but the outcome is a piece of art";
-
-	printf(text);
+	printf("%s",
+			"\"with proper grammar, but the outcome is a piece of art");
 	return (0);
 }
