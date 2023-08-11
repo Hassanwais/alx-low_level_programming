460a5298a853267190bb22ebb93a4d8a00cf9292
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-11 15:51:21 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - to print alphabets in lowercase
+ *
+ * Return: always (0)
+ *
+ */
+int main (void)
+{
+	char i = 'a';
+	while (i <= 'a')
+	{
+		putchar (i);
+		i++z;
+	}
+	putchar ("\n");
+	return (0);
+}
