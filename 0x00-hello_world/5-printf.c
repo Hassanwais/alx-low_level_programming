ab21ff93695990cf83f582b99784ffa814493c98
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-11 04:10:19 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - Entry point as the main function
  * Return: Always 0
  */
-int main(void)i
+int main(void)
 {
 	printf("%s",
 			"with proper grammar, but the outcome is a piece of art,");
