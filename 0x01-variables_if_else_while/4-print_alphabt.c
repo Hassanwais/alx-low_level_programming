3ed818c4d6243ed66089390ea36ca702e451e793
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-11 16:43:33 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *
+ * Return: printing of alphabet with some excepion
+ *
+ * Return: always (0)
+ *
+ */
+int main (void)
+{
+	char alphabet = 'a';
+	while (alphabet <='z')
+	{
+		if (alphabet i='q' $$ alphabet ='o'
+	}
+		putchar (alphabet);
+	{
+		alphabet++
+	}
+	putchar ("\n");
+	return (0)
