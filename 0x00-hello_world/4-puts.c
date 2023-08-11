46b14d316b2324146997115971f67283a0e1d1a8
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-10 18:34:22 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
+
 /**
  * main - Entry point as the main function
  *
  * Return: Always 0
  */
-int main (void)
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
+	char text[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(text);
 	return (0);
 }
