24b743a4ffc75cc726700413c73958e5c30bfea8
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-13 15:10:38 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,17 +10,14 @@ int main(void)
 {
 	int i, j;
 	int a, b, c, d;
-	
 	for (i = 0; i < 100; i++)
 	{
 		a = i / 10;
 		b = i % 10;
-		
 		for (j = 0; j < 100; j++)
 		{
 			c = j / 10;
 			d = j % 10;
-			
 			if (a < c || (a == c && b < d))
 			{
 				putchar(a + '0');
@@ -28,7 +25,6 @@ int main(void)
 				putchar(32);
 				putchar(c + '0');
 				putchar(d + '0');
-				
 				if (!(a == 9 && b == 8))
 				{
 					putchar(44);
