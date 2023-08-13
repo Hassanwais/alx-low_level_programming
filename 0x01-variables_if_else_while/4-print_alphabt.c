22a420419ef97b8b4f2457955facba54842c233e
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-13 16:53:31 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  *
  * Return: printing of alphabet with some excepion
  *
- * Return: always (0)
+ * Return: Always 0
  *
  */
 int main (void)
 {
 	char alphabet = 'a';
-	while (alphabet <='z')
+	while (alphabet <= 'z')
 	{
-		if (alphabet i='q' $$ alphabet ! ='o')
-	}
-		putchar(alphabet);
-	{
+		if (alphabet != 'q' && alphabet != 'e')
+		{
+			putchar(alphabet);
+		}
 		alphabet++;
 	}
-	putchar ("\n");
+	putchar ('\n');
 	return (0);
 }
