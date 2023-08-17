5affa2b54263b070eef8a238df2ac3b6e7e4220f
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-16 19:24:32 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * add - adds two values
+ * @n1: The first value to be computed
+ * @n2: the second value to be computed
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: -----
+ *
+ */
+void times_table(void)
+{
+	int a, b, c, d, e
+		for (a = 0; a <= 9; a++)
+		{
+			for (b = 0; b <= 9; b++)
+			{
+				c = a * b;
+				if (c > 9)
+				{
+					d = c % 10;
+					e = (c - d) / 10;
+					_putchar(44);
+					_putchar(32);
+					_putchar(e + '0');
+						_putchar(d + '0');
+				}
+				else
+				{
+					if (b != 0)
+					{
+						_putchar(44);
+						_putchar(32);
+						_putchar(32);
+					}
+					_putchar(c + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(44);
+					_putchar(32);
+				}
+			}
+			_putchar ('\n');
+		}
+}
+int main(void)
+{
+	times_table();
+	return (0);
+}
