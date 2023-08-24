002d22c2b7d9b5a028b45d912934a63cb114bc6d
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-08-24 16:34:19 -0700

    a function that capitalizes all words of a string.

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,33 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
 /**
- * cap_string - Capitalizes all words of a string.
+ * cap_string - Capitalizes the first letter of each word in a string.
  * @str: Pointer to the input string.
  * Return: Pointer to the modified string.
  */
 char *cap_string(char *str)
 {
 	int i = 0;
-	int capitalize_next = 1;
+	bool new_word = true;
 
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-	{
-		if (capitalize_next)
+		if (isalpha(str[i]))
 		{
-			str[i] = str[i] - ('a' - 'A');
-			capitalize_next = 0;
+			if (new_word)
+			{
+				str[i] = toupper(str[i]);
+				new_word = false;
+			}
 		}
-	}
-		else if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' ||
-				str[i] == ',' || str[i] == ';' || str[i] == '.' ||
-				str[i] == '!' || str[i] == '?' || str[i] == '"' ||
-				str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		else
 		{
-			capitalize_next = 1;
+			new_word = true;
 		}
+
 		i++;
 	}
 
