9781928cc29ab1117331017c7a4dc692e92887d6
Ahmed Hassan Kingweiss <ahmedhassanuwais1994@gmail.com>
2023-09-11 15:06:25 -0700

    a new type struct dog with the following elements:

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,22 @@
+0x0E. C - Structures, typedef
+
+
+0. Poppy
+mandatory
+
+
+Define a new type struct dog with the following elements:
+
+name, type = char *
+age, type = float
+owner, type = char *
+
+to 
+
+How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg
+mandatory
+
+
+Write a function that frees dogs.
+
+Prototype: void free_dog(dog_t *d);
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Represents information about a dog
+ * @name: Pointer to a string containing the dog's name
+ * @age: Age of the dog as a floating-point number
+ * @owner: Pointer to a string containing the dog's owner's name
+ *
+ * Description: This struct defines a data structure to store information
+ * about a dog, including its name, age, and owner's name.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
